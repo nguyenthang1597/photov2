fe84df13e97fc4c5705394061f8bfb986ccb5a53
Nguyen Thang <nguyenthang1597@gmail.com>
2018-10-23 17:45:27 +0700

    Updates

---
+++ precache-manifest.2c805966d3cb0a6e53c5da51fb6f4ec1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d6fa003eb1105bb8f03fada4e98640d",
+    "url": "/photov2/static/media/loading.3d6fa003.gif"
+  },
+  {
+    "revision": "48b0c02bb540b1e0683b",
+    "url": "/photov2/static/js/runtime~main.48b0c02b.js"
+  },
+  {
+    "revision": "36a229b9112ab69681ca",
+    "url": "/photov2/static/js/main.36a229b9.chunk.js"
+  },
+  {
+    "revision": "63a94056d285aa9c3d4e",
+    "url": "/photov2/static/js/1.63a94056.chunk.js"
+  },
+  {
+    "revision": "36a229b9112ab69681ca",
+    "url": "/photov2/static/css/main.b163d3b9.chunk.css"
+  },
+  {
+    "revision": "72d296447405408cec77a3df3af0e531",
+    "url": "/photov2/index.html"
+  }
+];
